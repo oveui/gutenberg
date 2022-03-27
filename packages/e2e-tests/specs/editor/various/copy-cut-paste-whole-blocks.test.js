c15fb75a55ce728522a6aede4370827cab82b194
Ella van Durpe <ella@vandurpe.com>
2022-03-27 15:08:41 +0300

    Try removing delays in e2e tests

--- packages/e2e-tests/specs/editor/various/copy-cut-paste-whole-blocks.test.js
+++ packages/e2e-tests/specs/editor/various/copy-cut-paste-whole-blocks.test.js
@@ -100,8 +100,6 @@ describe( 'Copy/cut/paste of whole blocks', () => {
 		await page.click( '.editor-block-list-item-paragraph' );
 		await page.keyboard.type( 'P' );
 		await page.keyboard.press( 'ArrowLeft' );
-		// Delay from writing-flow/use-keyboard-selection-observer.js
-		await page.evaluate( () => new Promise( requestAnimationFrame ) );
 		await page.keyboard.press( 'ArrowLeft' );
 		// Cut group.
 		await pressKeyWithModifier( 'primary', 'x' );
@@ -147,8 +145,6 @@ describe( 'Copy/cut/paste of whole blocks', () => {
 		await page.click( '.editor-block-list-item-paragraph' );
 		await page.keyboard.type( 'P' );
 		await page.keyboard.press( 'ArrowLeft' );
-		// Delay from writing-flow/use-keyboard-selection-observer.js
-		await page.evaluate( () => new Promise( requestAnimationFrame ) );
 		await page.keyboard.press( 'ArrowLeft' );
 		// Cut group.
 		await pressKeyWithModifier( 'primary', 'x' );
